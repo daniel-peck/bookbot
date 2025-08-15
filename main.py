97fc7eefb83202d001a45e8b102c529183474f71
daniel_peck <daniel.syre.peck@gmail.com>
2025-08-15 11:06:19 -0700

    updated main.py to count words

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("greetings boots")
+def count_words(text_from_book): 
+    return(len(text_from_book.split()))
+
+def get_book_text(filepath):
+    with open(filepath) as f: 
+        file_contents = f.read()
+    return file_contents
+
+def main():
+    print(get_book_text("/home/dan/workspace/github.com/daniel-peck/bookbot/books/frankenstein.txt"))
+
+
+
+print(f"{count_words(get_book_text("/home/dan/workspace/github.com/daniel-peck/bookbot/books/frankenstein.txt"))} words found in the document")
+#main()
+
+
+    
